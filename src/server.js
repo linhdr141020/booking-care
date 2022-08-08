95fbe00a3dcfcbd3b57971b4b82ac0315a0a63e9
linhdr141020 <81572858+linhdr141020@users.noreply.github.com>
2022-08-08 23:09:02 +0700

    1

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import viewEngine from './config/viewEngine'
+import initWebRoutes from './route/web'
+import connectDB from './config/connectDB'
+
+require('dotenv').config();
+
+let app = express();
+
+//config app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+viewEngine(app);
+initWebRoutes(app);
+
+connectDB();
+
+//Port == underfine => port = 6969
+const port = process.env.PORT || 6969;
+
+app.listen(port,() => {
+    console.log(`Backend Nodejs is running on the port ${port} `);
+})
