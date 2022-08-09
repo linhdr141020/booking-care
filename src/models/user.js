5a479fbbbeeb2c470cb6d0d4b268ca077f00692f
linhdr141020 <81572858+linhdr141020@users.noreply.github.com>
2022-08-09 15:40:32 +0700

    fix

--- src/models/user.js
+++ src/models/user.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
       firstName: DataTypes.STRING,
       lastName: DataTypes.STRING,
       address: DataTypes.STRING,
-      phomeNumber: DataTypes.STRING,
+      phoneNumber: DataTypes.STRING,
       gender: DataTypes.BOOLEAN,
       image: DataTypes.STRING,
       roleId: DataTypes.STRING,
