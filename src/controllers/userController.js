df5161b05e6f179a49b63843591c3a7912b730b9
linhdr141020 <81572858+linhdr141020@users.noreply.github.com>
2022-08-13 13:48:26 +0700

    api get all users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -18,6 +18,28 @@ let handleLogin = async (req, res) => {
     user: userData.user ? userData.user : {},
   });
 };
+
+let handleGetAllUsers = async (req, res) => {
+  let id = req.body.id;
+
+  if (!id) {
+    return res.status(200).json({
+      errCode: 1,
+      errMessage: "Missing require param",
+      users: [],
+    });
+  }
+
+  let users = await userService.getAllUsers(id);
+  console.log(users);
+  return res.status(200).json({
+    errCode: 0,
+    errMessage: "Ok",
+    users,
+  });
+};
+
 module.exports = {
   handleLogin,
+  handleGetAllUsers,
 };
